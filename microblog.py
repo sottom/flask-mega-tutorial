3620a473109f505d2015053d0c795bfa5c0a566e
Mitchell Sotto <sottochoro@gmail.com>
2019-08-22 11:14:56 -0600

    setup shell context

--- microblog.py
+++ microblog.py
@@ -1,2 +1,12 @@
 # you need to have a Python script at the top-level that defines the Flask application instance.
-from app import app
+from app import app, db
+from app.models import User, Post
+
+# The app.shell_context_processor decorator registers the function as a shell context function. When the flask shell command runs, it will invoke this function and register the items returned by it in the shell session.
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Post': Post
+    }
